03465f169ba31230ef6995c8952eecaf8b934feb
parktaegune <qkrxo876@naver.com>
2018-03-27 11:43:43 +0900

    (2018-03-27) init testjs

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,11 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = "Cloude Service";
 var text = 'This is a text.';
+text = 10.5;
+var id = prompt('ID 입력','');
+
+    alert(id+'가 로그인함.');
+    var passwordSystem = '12345';
+    var password = prompt('PW 입력','');
+
+}
